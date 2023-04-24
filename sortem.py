d3f8af63ed4cff429aa0223735969d1796afeec7
David Staab <david.staab@gmail.com>
2023-04-24 15:49:42 -0700

    explicit nltk download instruction

--- sortem.py
+++ sortem.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from shutil import move
 from time import sleep
 
-from nltk import word_tokenize
+from nltk import download as nltk_download, word_tokenize
 from pathvalidate import sanitize_filename
 
 from striprtf.striprtf.striprtf.striprtf import rtf_to_text
@@ -143,6 +143,8 @@ def run_single(n: int=-1, dry_run: bool=False):
         
 
 if __name__ == '__main__':  # Need this for mp to work!
+
+    nltk_download('punkt')
 
     print(f'Application directory: {APP_DIR}')
     for d in [SORTING_DIR, UNREADABLE_DIR, UNSAVEABLE_DIR]:
